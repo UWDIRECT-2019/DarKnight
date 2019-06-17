60e3ba9d36fe29b63a8b074817ad0d862a584886
Chih-Wei Hsu <cwh32@uw.edu>
2019-06-16 22:35:52 -0700

    Update unit tests

---
+++ darknight/test/processing/test_remove_other_elements.py
@@ -0,0 +1,35 @@
+# Import
+import pandas as pd
+import numpy as np
+from rdkit.Chem import AllChem as Chem
+from rdkit.Chem import PandasTools,Draw
+import math
+import openbabel
+import darkchem
+from IPython.display import display
+import sys
+sys.path.append("../..")
+import processing
+
+# Define testing function
+def test_remove_other_elements():
+    # Import some data to use
+    data = pd.read_excel('test_remove_other_elements.xlsx')
+
+    # Operate the function wihch is being tested
+    remove_other_elements = processing.remove_other_elements(data)
+
+    # Assert several arguments
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'F']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'l'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'B']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'r'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'I']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'i'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'M']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'g'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'L']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'b'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'a']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'e'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'K']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'V'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'd']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'R'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'Z']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'G'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'A']) == len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'Y'])
+    assert len(remove_other_elements.loc[remove_other_elements['Reactants'] == 'u']) == 0
+
+    return 0
---
+++ darknight/test/processing/test_split_reaction_pair.py
@@ -0,0 +1,27 @@
+# Import
+import pandas as pd
+import numpy as np
+from rdkit.Chem import AllChem as Chem
+from rdkit.Chem import PandasTools,Draw
+import math
+import openbabel
+import darkchem
+from IPython.display import display
+import sys
+sys.path.append("../..")
+import processing
+
+# Define testing function
+def test_split_reaction_pair():
+    # Import some data to use
+    data = pd.read_excel('test_split_reaction_pair.xlsx')
+
+    # Operate the function wihch is being tested
+    split_reaction_pair = processing.split_reaction_pair(data)
+
+    # Assert several arguments
+    assert len(data) == len(split_reaction_pair)
+    assert type(data) == type(split_reaction_pair)
+    assert split_reaction_pair.shape == (5,2)
+
+    return 0
